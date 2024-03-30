c12dbf41b5cdffdd176dcd9d2963c08e45147c25
Alina Kalarash <bloha7895@gmail.com>
2024-03-30 16:03:33 +0200

    it builds!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.print("<html><body>");
-        out.print("<h3>Hello World, I'm a new Servlet!");
+        out.print("<h3>Hello World, I'm a new Servlet!</h3>");
         out.print("</body></html>");
         out.close();
     }
