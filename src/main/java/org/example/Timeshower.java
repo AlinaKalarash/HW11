05a974b0d802c8fafd08617b35c76e817aec74d3
Alina Kalarash <bloha7895@gmail.com>
2024-03-30 18:11:38 +0200

    final

--- src/main/java/org/example/Timeshower.java
+++ src/main/java/org/example/Timeshower.java
@@ -7,17 +7,27 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.*;
 
-@WebServlet(value = "/simple")
-public class Main extends HttpServlet {
+@WebServlet(value = "/time")
+public class Timeshower extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String timezone = request.getParameter("timezone");
+        OffsetDateTime now;
+        if (timezone == null || timezone.length() == 3) {
+            now = OffsetDateTime.now(ZoneOffset.ofHours(0));
+        } else {
+            int time = Integer.parseInt(timezone.replace("UTC", "").replace(" ", ""));
+            now = OffsetDateTime.now(ZoneOffset.ofHours(time));
+        }
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.print("<html><body>");
         out.print("<h3>Hello World, I'm a new Servlet!</h3>");
+        out.print("<h1>current time is: " + now + "</h1>");
         out.print("</body></html>");
         out.close();
     }
-
 }
